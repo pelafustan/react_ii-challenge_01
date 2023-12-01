bcaa1110c7108e7d7b742680dbe1517ebd03a524
Patricio Parada <paradagpatricio@gmail.com>
2023-11-30 22:55:07 -0300

    add home view. I'm using tabs, I like tabs.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,28 @@
+import { Tabs } from "antd";
+import type { TabsProps } from 'antd';
+import PiesView from "./PiesView";
+import CakesView from "./CakesView";
+
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  );
+  const items: TabsProps["items"] = [
+    {
+      label: "Cakes",
+      key: "1",
+      children: <CakesView />
+    },
+    {
+      label: "Pies",
+      key: "2",
+      children: <PiesView />
+    },
+  ];
+
+  return(
+    <Tabs
+      defaultActiveKey="1"
+      type="card"
+      tabPosition="left"
+      items={items}
+    />
+  )
 }
