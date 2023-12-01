5aceb44251cefad43635f9f57bebd722dd546d90
Patricio Parada <paradagpatricio@gmail.com>
2023-11-30 22:56:09 -0300

    update NotFound view. After 5 seconds, you should be linked to the home page

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((t) => t - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (countdown <= 0) {
+      setCountdown(5);
+      navigate("/");
+    }
+  }, [countdown])
+
   return (
-    <h1>Not Found</h1>
-  );
+    <>
+      <div>
+        <h1>Not Found</h1>
+        <p>You will be redirected to the home page in {countdown} seconds...</p>
+      </div>
+    </>
+  )
 }
+
